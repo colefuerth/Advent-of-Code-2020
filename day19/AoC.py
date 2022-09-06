1fe2487bbaaeafb7422c967c70c1e05ad76b2de4
Cole Fuerth <colefuerth@gmail.com>
2022-09-06 16:18:03 -0400

    part 1 complete, day 19

--- day19/AoC.py
+++ day19/AoC.py
@@ -1,51 +1,55 @@
-from sys import argv
+from sys import argv, setrecursionlimit
 from itertools import product, count, permutations
 from copy import deepcopy
 import re
 
+# setrecursionlimit(30)
 
-def check_rule(rule: int, rules: list, string: str):
+
+def check_rule(rule: int, rules: list, string: str, level: int = -1, history: set = set()) -> str:
+    history = history.copy()
+    if rule in history:
+        return string
+    history.add(rule)
+    level += 1
+    padding = "| " * level
     if string == '':
         raise ValueError('String is empty')
+    # print(f'{padding}checking rule {rule}: {rules[rule]} with string {string}')
     rule = rules[rule]
     if type(rule) == str:
         return string[1:] if string.startswith(rule) else string
+    possibliities = []
     for branch in rule:
+        # print(f'{padding}checking branch {branch} against {string}')
         s = string
+        nochange = False
         for leaf in branch:
-            st = check_rule(leaf, rules, s)
+            st = check_rule(leaf, rules, s, level, history)
             if st == s:
+                nochange = True
                 break
             s = st
-        if s == '':
-            return s
-    return string
+        if not nochange:
+            possibliities.append(s)
+    return string if not possibliities else min(possibliities, key=len)
 
 
 def part1(f: list) -> int:
-    unparsed = []
-    strings = []
-    for i, line in enumerate(f):
-        if not line:
-            unparsed = f[:i]
-            strings = f[i + 1:]
-    rules = []
-    for i, line in enumerate(unparsed):
-        branch = re.findall(r"(\d[ \d]*)(?!:)", line)
-        if branch:
-            rules.append(
-                tuple(tuple(map(int, b.strip().split(" "))) for b in branch)
-            )
-            continue
+    strings = f[f.index('') + 1:]
+    rules = {}
+    for line in f[:f.index('')]:
+        i = int(line.split(':')[0])
         rule = re.findall(r"\"[^\"]*\"", line)
-        rules.append(rule[0][1:-1])
-
-    print(rules)
+        if rule:
+            rules[i] = rule[0][1:-1]
+            continue
+        branch = re.findall(r"(\d[ \d]*)(?!.*:)", line)
+        rules[i] = tuple(tuple(map(int, b.strip().split(" "))) for b in branch)
 
     results = []
     for s in strings:
         r = check_rule(0, rules, s)
-        print(s, r)
         results.append(r == '')
     return sum(results)
 
@@ -55,11 +59,11 @@ def part2(f: list) -> int:
 
 
 def main():
-    # start by getting file as a list of strings
-    f = [l.strip() for l in open(argv[1], "r")]
+    inpfile = argv[1] if len(argv) > 1 else "input.txt"
+    f = [l.strip() for l in open(inpfile, "r")]
 
     print("Part 1:", part1(deepcopy(f)))
-    print("Part 2:", part2(deepcopy(f)))
+    # print("Part 2:", part2(deepcopy(f)))
 
 
 if __name__ == "__main__":
